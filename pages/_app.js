a300d1d83c8a64dab0661b0a3e9cb80053ed680f
Elissandro Junior <teofiloeli@gmail.com>
2023-01-25 00:15:57 -0300

    add initial structure

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,28 @@
-import '@/styles/globals.css'
+import { PropTypes } from 'prop-types'
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/material/styles'
+import { CssBaseline } from '@mui/material'
+import theme from '../src/theme'
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function MyApp (props) {
+  const { Component, pageProps } = props
+
+  return (
+    <>
+      <Head>
+        <title>My page</title>
+        <meta name="viewport" content="minimun-scale-1, initial-scale-1, width-device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+        <CssBaseline />
+        <Component {...pageProps} />
+    </ThemeProvider>
+    </>
+)
+}
+
+MyApp.propTypes = {
+    Component: PropTypes.func.isRequired,
+    pageProps: PropTypes.object.isRequired,
 }
---
+++ src/theme.js
@@ -0,0 +1,23 @@
+import { Roboto } from '@next/font/google'
+import { createTheme } from '@mui/material/styles'
+
+export const roboto = Roboto({
+    weight: ['300', '400', '500', '700'],
+    subsets: ['latin'],
+    display: 'swap',
+    fallback: ['Helvetica', 'Arial', 'sans-serif'],
+  });
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: '#fac251'
+        },
+        secondary: {
+            main: '#F7F7F7'
+        },
+        
+    }
+})
+
+export default theme
